02d05b60fde3b33b1d55fefced6fd9f52acfd4e4
ShahzaibI <sg03056924122@gmail.com>
2024-08-07 12:05:07 +0500

    (17). Django Report Card Project

--- vege/models.py
+++ vege/models.py
@@ -25,6 +25,11 @@ class StudentID(models.Model):
     def __str__(self) -> str:
         return self.student_id
     
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.subject_name
+
 class Student(models.Model):
     department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
     student_id = models.OneToOneField(StudentID, related_name="studentid", on_delete=models.CASCADE)
@@ -39,3 +44,13 @@ class Student(models.Model):
     class Meta:
         ordering = ['student_name'] # for ordering data in database
         verbose_name = "student" # for table name in database
+
+class SubjectMarks(models.Model):
+    student = models.ForeignKey(Student, related_name="studentmarks", on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    marks = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.student.student_name}{self.subject.subject_name}"
+    class meta:
+        unique_together = ['student', 'subject']
